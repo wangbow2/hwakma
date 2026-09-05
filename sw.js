663b2e4427796a9ce73c94c191301201c80be310
황영환 <hwang-yeonghwan@hwang-yeonghwan-ui-Macmini.local>
2026-09-05 20:18:42 +0900

    기도 중심 기준과 인도자 연락망 추가

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // 배포할 때마다 이 값만 올리면 된다. 앱이 새 버전을 알아채는 유일한 기준.
 // index.html의 APP_BUILD와 반드시 같은 값이어야 한다.
-const APP_BUILD = '2026-09-05.10';
+const APP_BUILD = '2026-09-05.12';
 const CACHE_NAME = 'hwakma-' + APP_BUILD;
 const ASSETS = [
   './',
